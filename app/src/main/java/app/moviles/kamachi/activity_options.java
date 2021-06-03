41c51314eb636b55f34a8a3212982dd0346d803e
ProtSenpai <jdjimenez04@hotmail.com>
2021-06-03 15:21:08 -0500

    new activities

---
+++ app/src/main/java/app/moviles/kamachi/activity_options.java
@@ -0,0 +1,14 @@
+package app.moviles.kamachi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
