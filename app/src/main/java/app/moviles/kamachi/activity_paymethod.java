587e39e165b1c0121988dc90514bc442fcd8998d
ProtSenpai <jdjimenez04@hotmail.com>
2021-06-05 14:42:31 -0500

    new screens created

---
+++ app/src/main/java/app/moviles/kamachi/activity_paymethod.java
@@ -0,0 +1,14 @@
+package app.moviles.kamachi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_paymethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paymethod);
+    }
+}
